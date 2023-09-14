858420071118a47b94cbaf5a8285fdd1807d4e07
Poshikagnanavel <144425755+Poshikagnanavel@users.noreply.github.com>
2023-09-14 22:11:15 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-def factorial(n):
-  if(n==0 or n==1):
-    return 1
-  else:
-    return n*factorial(n-1)
-number=int(input("Enter the num value:"))
-res=factorial(number)
-print("The factorial of {} is {}.".format (number,res))
+#Define the basic class player 
+class player:
+  def play(self):
+    print("The player is playing cricket.") 
+
+# Define the derived class Batsman
+class Batsman:
+  def play(self):
+     print("The batsman is batting.")
+#Define the derived class Bowler
+class Bowler:
+  def play(self):
+    print("The bowler is bowling.")
+#Create objects of Batsman and Bouter classes
+batsman = Batsman()
+bowler =Bowler()
+#Call the play sethod for each object
+batsman.play()
+bowler.play()
